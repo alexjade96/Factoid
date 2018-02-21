c7908e1dbf3b5646383fe39d77d826de098ab02f
Alexander Yeh <alexjade08>
2018-02-21 13:17:54 -0800

    Psuedo Code

--- factoid.py
+++ factoid.py
@@ -39,22 +39,39 @@ def hist_comp(value):
 '''
 Store Recent High/Recent Low
 Jan 26 2018
-
+current-high = 0
+high-date = yyyymmdd
+For line in file:
+	if line[index-for-current-high] > current-high:
+		current-high = line[index-for-current-high]
+		high-date = line[date-for-current-high]
 Feb 11 2016
+current-low = 0
+low-date = yyyymmdd
+For line in file:
+	if line[index-for-current-low] > current-low:
+		current-low = line[index-for-current-low]
+		low-date = line[date-for-current-low]
 
-
-Jan 20 2017
+Jan 20 2017 (Trump Inaug)
 D9475
+23744.73
 Nov 8 2016
 D9426
+22165.78
 Dec 15 2015
 D9199
+21099.58
 Sept 12 2012
 D8380
+15038.58
 Aug 26 2010
 D7864
+10973.00
 Mar 9 2009
 D7493
+6858.43
 Oct 9 2007
 D7138
+15806.69
 '''
