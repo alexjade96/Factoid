7879393be6621118c9dec928efbd22ed28a86d6c
Alexander Yeh <alexjade08>
2018-03-01 11:56:18 -0800

    Adding some variables

--- factoid.py
+++ factoid.py
@@ -31,12 +31,11 @@ xl.sheet_names
 df = xl.parse(sheet1)
 df.head()
 
-dates = [
+comparisons = [
 	datetime.datetime.strftime("%Y%m%d"),
 	datetime.datetime.strftime("%Y%m01"),
 
 ]
-
 
 #df has -2 rows (For Column Title & index row 0)
 #print df.head()
@@ -47,18 +46,62 @@ print df.tail(22)
 #print df[:-22].dropna(thresh=2)
 
 #Use [Max - 27] to get latest Date
+
 '''
 1) Define & use formulas outside of xlsx worksheet/inside script
 2) Use COM/win32 to use existing Excel formulas (win dev only)
 3) Generate new table/spreadsheet with strict definition for easier data manip.
 '''
-
 #input: close value, date
 #use: historical values (yesterday, last month, last quarter)
 #output: Range of comparison values 
 
+'''
+#Jan 20 2017 "the close on the day of the Trump Inauguration"
+D9475
+23744.73
+Trump_Inaug = [20170120,23744.73,"the close on the day of the Trump Inauguration"]
+float(1.00 - Input/Trump_Inaug[0])
+
+#Nov 8 2016 "the close on the day of the 2016 Election"
+D9426
+22165.78
+Election = [20161108,22165.78,"the close on the day of the 2016 Election"]
+float(1.00 - Input/Election[0])
+
+#Dec 15 2015 "the close before the Federal Reserve raised interest rates for the first time since June 29, 2006"
+D9199
+21099.58
+Intrest_raise = [20151215,21099.58,"the close before the Federal Reserve raised interest rates for the first time since June 29, 2006"]
+float(1.00 - Input/Intrest_raise[0])
+
+#Sept 12 2012 "the close before Bernanke revealed QE3"
+D8380
+15038.58
+QE3 = [20120912,15038.58,"the close before Bernanke revealed QE3"]
+float(1.00 - Input/QE3[0])
+
+#Aug 26 2010 "the close before Bernanke revealed QE2"
+D7864
+10973.00
+QE2 = [20100826,10973.00,"the close before Bernanke revealed QE2"]
+float(1.00 - Input/QE2[0])
+
+#Mar 9 2009 "the financial Crisis low"
+D7493
+6858.43
+crisis_low = [20090309,6858.43,"the Financial Crisis low"]
+float(1.00 - Input/Crisis_low[0])
+
+#Oct 9 2007 "the old market high"
+D7138
+15806.69
+old_market_high = [20071009,15806.69,"the old Market High"]
+float(1.00 - Input/Old_Market_High[0])
+'''
 def close_compare(close_value, operand_value):
 	
+	return VAL_PERCENT, VAL_DOLLAR
 	#Return (percentage change, money value difference)
 
 def statement(VAL_DATE,VAL_PERCENT,VAL_DOLLAR):
@@ -75,11 +118,7 @@ datetime.datetime.strftime("%m")
 Quarter-quarter end
 if datetime.datetime.strftime("%m") % 3:
 	pre-val = get [datetime.datetime.strftime("%m") - 3]
-	
-'''
 
-def hist_comp(value):
-'''
 Store Recent High/Recent Low
 Jan 26 2018
 current-high = 0
@@ -95,33 +134,4 @@ For line in file:
 	if line[index-for-current-low] > current-low:
 		current-low = line[index-for-current-low]
 		low-date = line[date-for-current-low]
-
-Jan 20 2017 "the close on the day of the Trump Inauguration"
-D9475
-23744.73
-
-Nov 8 2016 "the close on the day of the 2016 Election"
-D9426
-22165.78
-
-Dec 15 2015 "the close before the Federal Reserve raised interest rates for the first time since June 29, 2006"
-D9199
-21099.58
-
-Sept 12 2012 "the close before Bernanke revealed QE2"
-D8380
-15038.58
-
-Aug 26 2010 "the close before Bernanke revealed QE3"
-D7864
-10973.00
-
-Mar 9 2009 "the financial Crisis low"
-D7493
-6858.43
-(20090309,6858.43)
-Oct 9 2007 "the old market high"
-D7138
-15806.69
-(20071009,15806.69)
 '''
